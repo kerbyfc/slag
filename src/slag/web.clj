51dde64e0b3fc6618034c98223e1a29e78e75d2d
kerbyfc <kerbyfc@gmail.com>
2013-10-17 20:04:07 +0400

    add arguments-checker

--- src/slag/web.clj
+++ src/slag/web.clj
@@ -12,10 +12,23 @@
   `(dosync
       (ref-set slag.web/routes (merge @slag.web/routes {~(keyword (clojure.string/replace route #"(/:)([\w]*)" "/$2")) (compojure.core/ANY (str "/" ~route) ~args (fn [request#] (liberator.core/run-resource request# ~@kvs)))}))))
 
+(map? (merge {} {:sd true}))
+
+(defn fname
+  [f]
+  (subs (first (re-find #"(\$)[A-Za-z0-9]*" (.toString f))) 1))
+
+(defn ensure-argument
+  [priority checker given & expected]
+  (let [priors ["first" "second" "third" "forth" "fifth" "sixth"]]
+    (if (not (checker given))
+      (throw (IllegalArgumentException. (str "Please, pass " (or (first expected) (fname checker)) " as " (get priors priority) " argument"))))))
+
 (defmacro defres
   [& form]
   (let [[root common route args res & nxt] (vec form)
-        factory `(res-handler ~(str root route) ~args ~(merge common res))]
+        factory `(res-handler ~(str root route) ~args (merge ~common ~res))]
+    (ensure-argument 1 string? root)
     (cond (nil? nxt) factory
           :else `(do
                    (defres ~root ~common ~@nxt)
@@ -27,19 +40,19 @@
         vars (vec (keep-indexed #(if (odd? %1) %2) bindings))
         values (vec (keep-indexed #(if (even? %1) %2) bindings))]
     (if (and (> (count values) 0) (= (count values) (count vars)))
-      `(fn [{$# :request}]
-         (apply (fn [~req ~@vars] ~(last kvs)) (apply conj [$#] (vec (map #(get-in $# %) ~values)))))
-      `(fn [{$# :request}] (apply (fn [~req] ~(last kvs)) [$#]))
+      `(fn [$#] (apply (fn [~req ~@vars] ~(last kvs)) (apply conj [$#] (vec (map #(get-in $# %) ~values)))))
+      `(fn [$#] (apply (fn [~req] ~(last kvs)) [$#]))
       )
     ))
 
+(def common-options {
+              :available-media-types ["text/plain"]
+              :allowed-methods [:get :put :post]
+           })
+
 (defres "servers"
 
-           {
-            :available-media-types ["text/plain"]
-            :allowed-methods [:get :put :post]
-            }
-
+          (merge common-options {:available-media-types ["text/plain" "text/html"]})
 
            "/:id" [id] {
                         :handle-ok (=> r
@@ -48,10 +61,23 @@
 
            "" [] {
 
-                  :exists? (=> r :query-params "a" a
-                             (= "asd" a))
+                  :exists? (=> ctx :request :query-params "choice" c
+                              (if (not (nil? (find {"1" "apple" "2" "orange"} c)))
+                                {:choice c}))
 
-                  :handle-ok (str "SERVERS LIST")
+
+                  :handle-not-found (=> r :choice c
+                                     (format "<html>There is no value for the option &quot;%s&quot;"
+                                             c))
+
+                  :handle-ok (=> ctx :choice c :representation :media-type mt
+                                 (condp = mt
+                                   "text/html" (format "<html><a href='#'>Your choice: &quot;%s&quot;</a>." c)
+                                   "text/plain" (format "Your choice: \"%s\".\n" c))
+                                   )
+
+                  :handle-not-acceptable "Uh, Oh, I cannot speak those languages!"
+
                   }
 
            )
